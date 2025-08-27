f1f1b27b89f3b51b3c4c631039bc31291b1eba31
mrnakordome <119735404+mrnakordome@users.noreply.github.com>
2025-08-27 18:49:07 +0000

    judge added

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,7 +11,7 @@ enum InputResult {
 func parseInput(_ input: String) -> InputResult {
     if input == "end" { return .end }
     if let n = Int(input) {
-        if n <= 6666 && n >= 0000{
+        if n <= 6666 && n >= 1000{
             var temp = n
             for _ in 1...4{
                 if(temp % 10) == 0 || (temp % 10) > 6{
@@ -25,6 +25,9 @@ func parseInput(_ input: String) -> InputResult {
     return .badInput
 }
 
+var secretBool = [Int](repeating: 0, count: 4)
+var guess = [Int](repeating: 0, count: 4)
+var guessBool = [Int](repeating: 0, count: 4)
 
 print("start")
 var secret = (0..<4).map { _ in Int.random(in: 1...6) }
@@ -47,7 +50,58 @@ outer: while true{
         break outer
     case .number(let n):
         print("number: \(n)")
+        var temp = n
         attempts += 1
+        var black = 0
+        var white = 0
+        for i in 0..<4{
+            guess[3-i] = temp % 10
+            temp /=  10
+            secretBool[i] = 0
+            guessBool[i] = 0
+        }
+        print(guess)
+        print(secret)
+        for i in 0..<4{
+            if secret[i] == guess[i]{
+                black+=1
+                secretBool[i] = 1
+                guessBool[i] = 1
+            }
+        }
+        if(black == 4){
+            print("You WIN")
+            print("total attemtps: \(attempts)")
+            print("secret code was: \(secretNumber)")
+            break outer
+        }
+        for i in 0..<4{
+            for j in 0..<4{
+                if guess[i] == secret[j] && guessBool[i] == 0 && secretBool[j] == 0{
+                    white+=1
+                    guessBool[i] = 1
+                    secretBool[j] = 1
+                }
+            }
+        }
+        print(secretBool)
+        print(guessBool)
+        print(black)
+        print(white)
+        var output = ""
+        for _ in 1...4{
+            if(black != 0){
+                output = output + "B"
+                black-=1
+            }
+        }
+        for _ in 1...4{
+            if(white != 0){
+                output = output + "W"
+                white-=1
+            }
+        }
+        print(output)
     case .badInput:
         print("bad input")
     }
